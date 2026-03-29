ed6baf517c22678b3c623f4823eba712ab3fdbc1
Omnia.Yehia <omniayehia6@outlook.com>
2026-03-29 21:10:25 +0200

    re-routing to base url after pay

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ActivatedRoute, RouterLink } from "@angular/router";
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CartService } from '../../core/services/cart/cart.service';
 
 @Component({
@@ -10,50 +10,44 @@ import { CartService } from '../../core/services/cart/cart.service';
   styleUrl: './checkout.component.css',
 })
 export class CheckoutComponent implements OnInit {
-  private readonly fb = inject(FormBuilder)
-  private readonly activatedRoute = inject(ActivatedRoute)
-  private readonly cartService = inject(CartService)
+  private readonly fb = inject(FormBuilder);
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly cartService = inject(CartService);
 
   checkoutForm!: FormGroup;
-  cartId :WritableSignal<string | null>= signal('')
+  cartId: WritableSignal<string | null> = signal('');
 
   ngOnInit(): void {
     this.checkoutForm = this.fb.group({
       details: [null, [Validators.required]],
       phone: [null, [Validators.required]],
       city: [null, [Validators.required]],
-    })
+    });
 
     this.activatedRoute.paramMap.subscribe({
-      next:(urlPath)=>{
-        this.cartId.set(urlPath.get('cartId'))
-      }
-    })
+      next: (urlPath) => {
+        this.cartId.set(urlPath.get('cartId'));
+      },
+    });
   }
 
-
-  checkoutSession(){
+  checkoutSession() {
     console.log(this.checkoutForm.value);
     let payload = {
-      shippingAddress : this.checkoutForm.value
-    }
+      shippingAddress: this.checkoutForm.value,
+    };
 
-
-    
-    this.cartService.checkOutSession(this.cartId() , payload).subscribe({
+    this.cartService.checkOutSession(this.cartId(), payload).subscribe({
       next: (res) => {
         console.log(res);
-        if(res.status == 'success'){
+        if (res.status == 'success') {
           console.log('entered if condition');
-          
-          window.open(res.session.url , '_self')
+          window.open(window.location.origin, '_self');
         }
       },
       error: (err) => {
         console.log(err);
-      }
+      },
     });
   }
-
-
 }
